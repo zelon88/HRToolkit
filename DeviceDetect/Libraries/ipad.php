db63b6a79e54adff6163cb3912ef8e13e39fc064
zelon88 <zelon88@gmail.com>
2016-07-04 13:45:42 -0400

    Update ipad.php

--- DeviceDetect/Libraries/ipad.php
+++ DeviceDetect/Libraries/ipad.php
@@ -26,9 +26,9 @@ if (screen.width = 768) {
 // ]]>
 <?php
  
-// / This file will load a static page and return a specified div.
-// / The strings for $divStart and $divEnd must by IDENTICAL to the
-// / way they are displayed in the $divLocation. 
+// This file will load a static page and return a specified div.
+// The strings for $divStart and $divEnd must by IDENTICAL to the
+// way they are displayed in the $divLocation. 
 $divLocation = file_get_contents('https://en.wikipedia.org/wiki/IPad_2');
 $divStart = '{?><table class=infobox hproduct vevent"<?php}';
 $divEnd = '{?></table><?php}';
