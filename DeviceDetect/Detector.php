dba8e81bdfe65e17117651be151ffb2a61071bbc
zelon88 <zelon88@gmail.com>
2016-07-04 22:04:03 -0400

    Fixed errors

--- DeviceDetect/Detector.php
+++ DeviceDetect/Detector.php
@@ -1,69 +1,28 @@
 <?php
-$UA = strtolower($_SERVER['HTTP_USER_AGENT']);
-$IPad1 = strtolower('iPad; U;');
-$IPad2and3 = strtolower('iPad; CPU');
-if (preg_match("/$UA/", $IPad1)) {
- 
-// / This file will load a static page and return a specified div.
-// / The strings for $divStart and $divEnd must by IDENTICAL to the
-// / way they are displayed in the $divLocation. 
-
-$divLocation = file_get_contents('https://en.wikipedia.org/wiki/IPad_(1st_generation)#cite_note-AppleIPadSpecs-1');
-$divStart = '{?><table class=infobox hproduct vevent"<?php}';
-$divEnd = '{?></table><?php}';
-$delimiter = '#';
-$divData = $delimiter . preg_quote($divStart, $delimiter) 
-                    . '(.*?)' 
-                    . preg_quote($divEnd, $delimiter) 
-                    . $delimiter 
-                    . 's';
-preg_match($divData,$divLocation,$div1);
-$div = $div1[0];
-// / Look ma, no CuRL!
-echo $div; }
-if (preg_match("/$UA/", $IPad2and3)) { ?>
-<script type="text/javascript">// < ![CDATA[
-if (screen.width = 768) { 
-// ]]>
-<?php
- 
-// This file will load a static page and return a specified div.
-// The strings for $divStart and $divEnd must by IDENTICAL to the
-// way they are displayed in the $divLocation. 
-$divLocation = file_get_contents('https://en.wikipedia.org/wiki/IPad_2');
-$divStart = '{?><table class=infobox hproduct vevent"<?php}';
-$divEnd = '{?></table><?php}';
-$delimiter = '#';
-$divData = $delimiter . preg_quote($divStart, $delimiter) 
-                    . '(.*?)' 
-                    . preg_quote($divEnd, $delimiter) 
-                    . $delimiter 
-                    . 's';
-preg_match($divData,$divLocation,$div1);
-$div = $div1[0];
-// / Look ma, no CuRL!
-echo $div; ?>
-} </script>
-<script type="text/javascript">// < ![CDATA[
-if (screen.width = 1536) { 
-// ]]>
-<?php
- 
-// This file will load a static page and return a specified div.
-// The strings for $divStart and $divEnd must by IDENTICAL to the
-// way they are displayed in the $divLocation. 
-$divLocation = file_get_contents('https://en.wikipedia.org/wiki/IPad_(3rd_generation)');
-$divStart = '{?><table class=infobox hproduct vevent"<?php}';
-$divEnd = '{?></table><?php}';
-$delimiter = '#';
-$divData = $delimiter . preg_quote($divStart, $delimiter) 
-                    . '(.*?)' 
-                    . preg_quote($divEnd, $delimiter) 
-                    . $delimiter 
-                    . 's';
-preg_match($divData,$divLocation,$div1);
-$div = $div1[0];
-// / Look ma, no CuRL!
-echo $div; ?>
-} </script>
-<?php }
+ -// / The DeviceDetect application and repo are the result of a Reddit 		
+ -// / thread where someone said it wasn't possible to detect, determine,		
+ -// / or even extrapolate at the hardware in-use by a client from the server.		
+ -// / 		
+ -// / This code will attempt to identify a user based on the detected user agent		
+ -// / and will redirect the user to a page containing specific information for 		
+ -// / their device. 		
+ -// / This code could be written to verify the user agent against the requested		
+ -// / display resolution as a method for detecting spoofed UA strings.		
+ -// / 		
+ -// / In the event that the device could not be identified we get as close to 		
+ -// / a match as we can before asking the user to install a script that will		
+ -// / work to complete the identification.		
+$UA = strtolower($_SERVER['HTTP_USER_AGENT']);		  $UA = strtolower($_SERVER['HTTP_USER_AGENT']);
+$mobileArr = array('E6782', 'C811', 'Iphone', 'Ipad', 'whatever');		 +$IPad1 = strtolower('iPad; U;');
+$deskArr = array('Windows', 'Linux');		 +$IPad2and3 = strtolower('iPad; CPU');
+$mc = 0;		 +if (preg_match("/$UA/", $IPad1)) {
+foreach ($mobileArr as $mobileArrs) {		 + 
+$mobileArrs = strtolower($mobileArrs);		 +// / This file will load a static page and return a specified div.
+if (preg_match("/$mobileArrs/", $UA)) {		 +// / The strings for $divStart and $divEnd must by IDENTICAL to the
+$mc++;		 +// / way they are displayed in the $divLocation. 
+include('/Libraries/'.$mobileArrs.'.php'); } }		 +
+if ($mc < 1) {		 +$divLocation = file_get_contents('https://en.wikipedia.org/wiki/IPad_(1st_generation)#cite_note-AppleIPadSpecs-1');
+foreach ($deskArr as $desktopArrs) {		 +$divStart = '{?><table class=infobox hproduct vevent"<?php}';
+$desktopArrs = strtolower($desktopArrs);		 +$divEnd = '{?></table><?php}';
+if (preg_match("/$desktopArrs/", $UA)) {		 +$delimiter = '#';
+ include('/Libraries/'.$desktopArrs.'.php'); } } }
