81bfc75b154c0f23e0c433271d0152b22370e30b
zelon88 <zelon88@gmail.com>
2016-07-04 20:18:53 -0400

    Fixed errors

--- DeviceDetect/Detector.php
+++ DeviceDetect/Detector.php
@@ -12,17 +12,18 @@
 // / In the event that the device could not be identified we get as close to 
 // / a match as we can before asking the user to install a script that will
 // / work to complete the identification.
+<?php
 $UA = strtolower($_SERVER['HTTP_USER_AGENT']);
 $mobileArr = array('E6782', 'C811', 'Iphone', 'Ipad', 'whatever');
 $deskArr = array('Windows', 'Linux');
 $mc = 0;
 foreach ($mobileArr as $mobileArrs) {
 $mobileArrs = strtolower($mobileArrs);
-if (preg_match($mobileArrs, $UA)) {
+if (preg_match("/$mobileArrs/", $UA)) {
 $mc++;
 include('/Libraries/'.$mobileArrs.'.php'); } }
 if ($mc < 1) {
-foreach ($desktopArr as $desktopArrs) {
+foreach ($deskArr as $desktopArrs) {
 $desktopArrs = strtolower($desktopArrs);
-if (preg_match($desktopArrs, $UA) {
+if (preg_match("/$desktopArrs/", $UA)) {
 include('/Libraries/'.$desktopArrs.'.php'); } } }
